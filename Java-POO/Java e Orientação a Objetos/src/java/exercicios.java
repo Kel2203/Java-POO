e93693b12ecc3aca464e1f93df853a1c355bd3a6
Raquel Barreto <78867830+Kel2203@users.noreply.github.com>
2023-04-15 19:03:42 -0300

    Update exercicios.java

--- Java-POO/Java e Orientação a Objetos/src/java/exercicios.java
+++ Java-POO/Java e Orientação a Objetos/src/java/exercicios.java
@@ -1,7 +1,7 @@
 package aula_java_project;
 import java.util.Scanner;
 public class exercicios_01 {
-
+ //Exercicio 01
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
