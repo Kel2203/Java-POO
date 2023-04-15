847a0f38c3018df3f352fa5c03d9c45a442cb08a
kel2203 <raquelsb2001@gmail.com>
2023-04-15 20:09:30 -0300

    exercício 01

---
+++ Java e Orientação a Objetos/src/application/Program.java
@@ -0,0 +1,29 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+import entities.Rectangle;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// EXERCICIO 01
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Rectangle r = new Rectangle();
+		
+		System.out.println("Entre com a largura e altura do retângulo:");
+		r.width = sc.nextDouble();
+		r.height = sc.nextDouble();
+		
+		System.out.printf("AREA = %.2f%n", r.area());
+		System.out.printf("PERÍMETRO = %.2f%n", r.perimeter());
+		System.out.printf("DIAGONAL = %.2f%n", r.diagonal());
+		
+		
+		sc.close();
+	}
+
+}
