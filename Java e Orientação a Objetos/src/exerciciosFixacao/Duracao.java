03cb20abf6daca7217ab2d2d1265c06900a6609d
kel2203 <raquelsb2001@gmail.com>
2023-09-02 14:11:08 -0300

    Exercicio Duração estrutura sequencial

--- Java e Orientação a Objetos/src/exerciciosFixacao/Duracao.java
+++ Java e Orientação a Objetos/src/exerciciosFixacao/Duracao.java
@@ -1,2 +1,26 @@
-package exerciciosFixacao;public class Duracao {
+package exerciciosFixacao;
+
+import java.util.Scanner;
+
+public class Duracao {
+    public static void main(String[] args) {
+//        Problema "duracao"
+//        Correção: https://github.com/acenelio/curso-algoritmos/blob/master/java/duracao.java
+//        Fazer um programa para ler uma duração de tempo em segundos, daí imprimir na tela esta duração no
+//        formato horas:minutos:segundos.
+
+        Scanner sc = new Scanner(System.in);
+        int duracao, horas, minutos, segundos, resto;
+
+        System.out.print("Digite a duracao em segundos: ");
+        duracao = sc.nextInt();
+
+        horas = duracao / 3600;
+        resto = duracao % 3600;
+
+        minutos = resto / 60;
+        segundos = resto % 60;
+
+        System.out.println(horas + ":" + minutos + ":" + segundos);
+    }
 }
